dd55b4869898c6e53799c45f963d91421c49ef5b
Grigoriy <gri003p@mail.ru>
2023-12-26 01:57:49 +0300

    added id to word

--- easyLanguage/Network/UIModels/WordUIModel.swift
+++ easyLanguage/Network/UIModels/WordUIModel.swift
@@ -11,4 +11,5 @@ struct WordUIModel {
     let categoryId: String // Связующий идентификатор для связи с категорией
     let translations: [String: String]
     let isLearned: Bool
+    let id: String // для лайка
 }
