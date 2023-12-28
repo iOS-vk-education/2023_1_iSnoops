b13b438582d9442e92244ecd251557517698cf79
Alexandra Kurganova <axndkgn@icloud.com>
2023-12-28 18:28:59 +0300

    fix kek

--- easyLanguage/Application/SceneDelegate.swift
+++ easyLanguage/Application/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        window.rootViewController = TabBarController()
+//        window.rootViewController = TabBarController()
         
         self.window?.makeKeyAndVisible()
         
