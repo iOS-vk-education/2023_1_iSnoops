2e11b51b8da749fc352ce94c0ceace8c9427a56a
Matvei Matiushko <55161938+ma14ew@users.noreply.github.com>
2024-05-15 18:28:23 +0300

    add improv

--- easyLanguage/CoreData/CoreDataService.swift
+++ easyLanguage/CoreData/CoreDataService.swift
@@ -10,17 +10,14 @@ import CoreData
 
 class CoreDataService {
     let persistentContainer = NSPersistentContainer(name: "CdModel")
-
     lazy var fetchedResultsController: NSFetchedResultsController<WordCDModel> = {
         let request = WordCDModel.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: #keyPath(WordCDModel.isLearned), ascending: true)
         request.sortDescriptors = [sortDescriptor]
-
-        let frc = NSFetchedResultsController(fetchRequest: request,
-                                             managedObjectContext: self.persistentContainer.viewContext,
-                                             sectionNameKeyPath: nil,
-                                             cacheName: nil)
-        return frc
+        return  NSFetchedResultsController(fetchRequest: request,
+                                           managedObjectContext: self.persistentContainer.viewContext,
+                                           sectionNameKeyPath: nil,
+                                           cacheName: nil)
     }()
 
     func loadStore() {
@@ -28,7 +25,6 @@ class CoreDataService {
             if let error = error {
                 print("unable to load")
                 print("\(error), \(error.localizedDescription)")
-
             } else {
                 do {
                     try self.fetchedResultsController.performFetch()
@@ -41,8 +37,8 @@ class CoreDataService {
 
     func saveWordToCoreData(model: WordApiModel) {
         let wordToCoreData = WordCDModel.init(entity: NSEntityDescription.entity(forEntityName: "WordCDModel",
-                                                                            in: persistentContainer.viewContext)!,
-                                         insertInto: persistentContainer.viewContext)
+                                                                                 in: persistentContainer.viewContext)!,
+                                              insertInto: persistentContainer.viewContext)
         wordToCoreData.id = model.id
         wordToCoreData.categoryId = model.categoryId
         wordToCoreData.isLearned = model.isLearned
